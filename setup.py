c8d81b8c82febafcc991ea0f5401837ef4d649b4
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2021-06-09 13:58:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'snow_pyrepl',         # How you named your package folder (MyLib)
   packages = ['snow_pyrepl'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.6',      # Start with a small number and increase it with every change you make
   license='GNU GPL v 3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Interact with Replit\'s API',   # Give a short description about your library
   author = 'SnowCoder',                   # Type in your name
   author_email = 'not@available.pleasedontemailme',      # Type in your E-Mail
   url = 'https://github.com/CoolCoderSJ/pyrepl',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/CoolCoderSJ/pyrepl/archive/refs/tags/v0.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/CoolCoderSJ/pyrepl/archive/refs/tags/v0.6.tar.gz',    # I explain this later on
   keywords = ['pyrepl', 'Python', 'Replit'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
 		  'protobuf',
